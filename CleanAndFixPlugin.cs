61bae6601d80fe831a859764de98fc6f62a9cca1
Mathieu Bourgois <bourgois.mathieu@gmail.com>
2015-06-08 19:04:40 +0200

    InitialPlugin
    Clean plugin with a DEBUG command test for Autocad 2016

---
+++ CleanAndFixPlugin.cs
@@ -0,0 +1,20 @@
+﻿// (C) Copyright 2015 by  Knowledge Corp
+//
+
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: ExtensionApplication(typeof(CleanAndFix.CleanAndFixPlugin))]
+
+namespace CleanAndFix
+{
+    public class CleanAndFixPlugin : IExtensionApplication
+    {
+        void IExtensionApplication.Initialize()
+        {
+        }
+
+        void IExtensionApplication.Terminate()
+        {
+        }
+    }
+}
---
+++ Tools/DEBUG.cs
@@ -0,0 +1,20 @@
+﻿//TODO DELETE THIS FILE
+
+using Autodesk.AutoCAD.Runtime;
+using CleanAndFix.Annotations;
+using CleanAndFix.Tools;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+[assembly: CommandClass(typeof(Debug))]
+
+namespace CleanAndFix.Tools
+{
+    class Debug
+    {
+        [CommandMethod("DEBUG", "DEBUG", CommandFlags.Transparent), UsedImplicitly]
+        public void DebugCommand()
+        {
+            Application.ShowAlertDialog("DEBUG");
+        }
+    }
+}
